772f61658cdc4d9a4568f06bd92d9313bc51daf9
ariefzuhri <ariefzuhri72@gmail.com>
2023-01-15 17:01:36 +0700

    Improve logo

--- src/common/components/Logo.js
+++ src/common/components/Logo.js
@@ -5,12 +5,15 @@ import { Box, Flex, Image, Text } from '@chakra-ui/react'
 export default function Logo(props) {
     return (
         <Flex align='center'>
-            <Image h='48px' src={logo}></Image>
+            <Image h={['56px', '72px', '72px', '72px']} src={logo}></Image>
             <Box px='16px' {...props}>
-                <Text fontSize='sm' fontWeight='bold'>
+                <Text fontSize={['14px', '18px', '18px', '18px']} fontWeight='bold'>
                     Desa Mertelu
                 </Text>
-                <Text fontSize='xs' fontWeight='normal'>
+                <Text fontSize={['10px', '12px', '12px', '12px']} fontWeight='normal'>
+                    Kapanewon Gedangsari
+                </Text>
+                <Text fontSize={['10px', '12px', '12px', '12px']} fontWeight='normal'>
                     Kabupaten Gunungkidul
                 </Text>
             </Box>
