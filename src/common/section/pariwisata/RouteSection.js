f0513a8672c9d2c8fd21016bd46fb1d43f115315
ariefzuhri <ariefzuhri72@gmail.com>
2023-01-23 11:04:14 +0700

    Add title to Google Maps frame

--- src/common/section/pariwisata/RouteSection.js
+++ src/common/section/pariwisata/RouteSection.js
@@ -27,6 +27,7 @@ export default function RouteSection() {
                     p={['16px', '20px', '28px', '32px']}
                 >
                     <iframe
+                        title='Green Village Gedangsari'
                         src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.7800352014306!2d110.61338231472588!3d-7.8130939943698525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a4f32bf83986b%3A0x89cfc63831ddf2a3!2sGreen%20Village%20Gedangsari!5e0!3m2!1sen!2sid!4v1673231908603!5m2!1sen!2sid'
                         style={{
                             width: '100%',
