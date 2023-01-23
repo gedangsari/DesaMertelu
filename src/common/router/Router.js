14649652806d60125e078c36ec3c519e9ab8c5f1
ariefzuhri <ariefzuhri72@gmail.com>
2023-01-23 11:40:22 +0700

    Add simple not found page

--- src/common/router/Router.js
+++ src/common/router/Router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Main from '../../page/Main';
+import NotFound from '../../page/NotFound';
 import Umkm from '../../page/Umkm';
 import { Route, Routes } from 'react-router-dom';
 
@@ -7,9 +8,10 @@ export default function Router() {
     return (
         <Routes>
             <Route exact path="/" element={<Main />} />
+            <Route path="/home" element={<Main />} />
             <Route path="/pariwisata" element={<Main />} />
             <Route path="/umkm" element={<Umkm />} />
-            <Route path="*" element={<Main />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 };
--- src/page/NotFound.js
+++ src/page/NotFound.js
@@ -1,10 +1,25 @@
 import Layout from '../common/layout/Layout';
 import React from 'react';
+import { Button, Text, VStack } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
     return (
         <Layout>
-            <h1>Page Not Found :(</h1>
+            <VStack h='100vh' justifyContent='center'>
+                <Text
+                    fontWeight='bold'
+                    fontSize='xl'
+                    mb={8}
+                >
+                    Halaman Tidak Ditemukan :(
+                </Text>
+                <Link to='/home'>
+                    <Button>
+                        Kembali ke Beranda
+                    </Button>
+                </Link>
+            </VStack>
         </Layout>
     );
 };
